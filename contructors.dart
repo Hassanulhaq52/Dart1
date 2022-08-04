60323bc936e63c94e87e2e990ed8a89bae51aad6
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-08-04 18:11:24 +0500

    constructors in dart

---
+++ contructors.dart
@@ -0,0 +1,21 @@
+void main() {
+
+  DartTest().dartFn();
+
+}
+
+class DartTest {
+  
+DartTest(){
+
+print('I am a dart Constructor');
+
+}
+
+void dartFn(){
+
+print('I am a Dart Function');
+
+  }
+  
+}
