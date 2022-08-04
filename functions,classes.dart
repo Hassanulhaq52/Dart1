2a9217a97d9bb1ea5c55c1af119830db350c72b5
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-08-04 17:33:25 +0500

    Variables is dart

---
+++ functions,classes.dart
@@ -0,0 +1,57 @@
+// void main() {
+
+//  Sixth().cricket();
+
+// }
+
+
+// class Sixth {
+
+//   void cricket(){
+
+// var whoIsCricketer = 'Hassan is a good Cricketer';
+// print(whoIsCricketer);
+
+//   }
+  
+// void painting(){
+
+
+// }
+
+// void coding(){
+
+
+// }
+
+// }
+
+
+
+
+
+// void main(){
+
+// var value = sum().add();
+// print(value);
+
+// }
+
+// class sum{
+
+// int adding(int b , int c, int d) {
+
+// return b + c + d;
+
+// }
+
+// add() {
+
+//  int a = adding(7, 8, 9);
+
+//   print("The sum is: $a");
+
+// }
+
+
+// }
---
+++ variables.dart
@@ -0,0 +1,17 @@
+main() {
+  
+int boxOfPens = 10;
+String name = 'Hassan Ul Haq';
+print(boxOfPens);
+hassan();
+print(name);
+
+
+
+}
+
+void hassan(){
+
+print('Hassan is a Good Boy');
+
+}
